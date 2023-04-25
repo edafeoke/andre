53b954d0eb7ab1575b18ab65b1f8dafaab598a8a
Edafe Oke Great <greatedafeoke@gmail.com>
2023-04-25 12:24:49 +0100

    Added some files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sideBar = document.querySelector(".sidebar");
+const dropdownButton = document.querySelector("#dropdown-button");
+const dropdown = document.querySelector("#dropdown");
+
+
+const closeLogo =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
+const openLogo =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg>';
+dropdownButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(e) {
+  if (sideBar.classList.contains("open")) {
+    sideBar.classList.remove("open");
+    dropdownButton.innerHTML = openLogo;
+  } else {
+    sideBar.classList.add("open");
+    dropdownButton.innerHTML = closeLogo;
+  }
+}
